7a547d5ece20079cd766b5cc974884291eb2b879
ishakson <ishaksylmz7@gmail.com>
2025-01-08 02:15:10 +0300

    main

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const ulke = "turkiye";
+const kita = "asia";
+let nufus = 85;
+const adaMi = true;
+const dil = "turkce";
+
+const yarisi_nufus = nufus / 2;
+const arti_bir_nufus = nufus++;
+const fazlaMi_6 = nufus > 6;
+const azMi_33 = nufus < 33;
+
+const description = `
+Ülke: ${ulke}
+Kıta: ${kita}
+Nufus: ${nufus}
+Nufus Yarısı: ${yarisi_nufus}
+Nufus Artı Bir: ${arti_bir_nufus}
+Fazla Mi 6: ${fazlaMi_6}
+Az Mi 33: ${azMi_33}
+Dil: ${dil}
+Ada Mi: ${adaMi}
+
+`;
+
+console.log(description);
